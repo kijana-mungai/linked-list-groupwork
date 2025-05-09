b38724a589f98b1486bba04bb42e0dd72faa14eb
khillon-makwana <khillon2005@gmail.com>
2025-05-09 10:09:22 +0300

    Main class

---
+++ main.py
@@ -0,0 +1,14 @@
+from LinkedList import LinkedList
+
+l1 = LinkedList()
+
+l1.append(15)
+l1.append(25)
+l1.append(28)
+l1.display()
+
+l1.insert_at_position(2, 30)
+l1.display()
+
+l1. delete_at_position(1)
+l1.display()
